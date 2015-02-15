6980d2ca215da2d482071b1f673258b84a2a1105
emthomas <titbabthomas@hotmail.com>
2015-02-15 00:49:03 -0500

    Added static pages
    -Added Wedding Info page
    -Added random picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Sam & Seb"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
+  def random_pic
+    num = 1 + rand(12)
+    "pic_#{num}.jpeg"
+  end
+
 end
