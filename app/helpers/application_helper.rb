a7a17ded81115c7793657d947e118874c331d021
Vagrant <titbabthomas@gmail.com>
2015-07-19 00:41:33 +0000

    Add time until next request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,4 +52,21 @@ module ApplicationHelper
     Table.joins("LEFT OUTER JOIN users on users.table_id = tables.id").group(:number).having("count(*)<max(capacity)")
   end
   
+  def time_to_request
+    start_time = Time.now
+	end_time = (start_time + 1.day).beginning_of_day()
+    
+    seconds_diff = (start_time - end_time).to_i.abs
+
+    hours = seconds_diff / 3600
+    seconds_diff -= hours * 3600
+
+    minutes = seconds_diff / 60
+    seconds_diff -= minutes * 60
+
+    seconds = seconds_diff
+
+    "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+  end
+  
 end
